052ac549101475de40ea9bf717ebb58676473782
dhiogo correa <dhiogo.correa@itera.com.br>
2019-02-16 15:18:30 -0200

    created customer service implementation

---
+++ facefy-api/src/main/java/com/facefy/facefy_api/services/impl/CustomerServiceImpl.java
@@ -0,0 +1,14 @@
+package com.facefy.facefy_api.services.impl;
+
+import com.facefy.facefy_api.models.Customer;
+import com.facefy.facefy_api.services.CustomerService;
+
+public class CustomerServiceImpl implements CustomerService {
+
+	@Override
+	public Customer getMe(long userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
