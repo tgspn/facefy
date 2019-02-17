9a399bfb561ac7d6e2834f649f42f12386230589
jonas <jonas@itera.com.br>
2019-02-17 03:02:57 -0300

    ta funfando e fodase

--- image_process/process_queue.py
+++ image_process/process_queue.py
@@ -6,6 +6,7 @@ import multiprocessing
 import time
 import os
 from multiprocessing import Pool
+import b64_img
 import api
 import json
 
@@ -19,15 +20,19 @@ custumers_encoing_img_database = list()
 
 def match_faces(img_full_path): 
     global custumers_encoing_img_database
-    print(img_full_path)
     img = cv2.imread(img_full_path,1)
     small_img = cv2.resize(img, (0, 0), fx=float(1/velocidade_proc), fy=float(1/velocidade_proc))
     face_locations = face_recognition.face_locations(small_img)
     face_encodings = face_recognition.face_encodings(small_img, face_locations)
     for face_encoding in face_encodings:
         for img_encoing_database in custumers_encoing_img_database:
-            matche = face_recognition.compare_faces([img_encoing_database['b64']], face_encoding)
-            print(matche)
+            try:
+                matche = face_recognition.compare_faces([img_encoing_database['b64']], face_encoding)
+                print(matche)
+                # if(matche):
+                #     print("achou o ",img_encoing_database['id'])
+            except:
+                print("nao eh foto ")
     os.remove(img_full_path)
 
 
@@ -37,10 +42,23 @@ def load_img_from_Database():
     global event_id
     headers = {'Content-Type': 'application/json','Master-Key': 'facefyiscool'}
     json_data = api.get_account_info(headers=headers,event_id=event_id)
-    print('customers')
     for customers in json_data['customers']:
-        custumers_encoing_img_database.append({'id':customers['customerId'],'b64':customers['base64Photo']})
-        print(customers['customerId'],customers['base64Photo'])
+        print(customers['base64Photo'])
+        img_base64 = b64_img.readb64(customers['base64Photo'])
+        if(img_base64 is not None):
+            small_img_detect = cv2.resize(img_base64, (0, 0), fx=float(1/velocidade_proc), fy=float(1/velocidade_proc))
+            face_encoding = face_recognition.face_encodings(small_img_detect)
+            if(len(face_encoding)>0):
+                print('TAMANHO: ',len(face_encoding))
+                custumers_encoing_img_database.append({'id':customers['customerId'],'b64':face_encoding[0]})
+            else:
+                dhiogo_image = face_recognition.load_image_file("dhiogo.jpg")
+                small_dhiogo = cv2.resize(dhiogo_image, (0, 0), fx=float(1/4), fy=float(1/4))
+                dhiogo_face_encoding = face_recognition.face_encodings(small_dhiogo)[0]
+                custumers_encoing_img_database.append({'id':'dhiogo','b64':dhiogo_face_encoding})
+                print(customers['customerId'],' face nao detectada')
+        else:
+            print(customers['customerId'],' nao tem foto correta')
     #fake data
     # for i in range(0,100):
     #     dhiogo_image = face_recognition.load_image_file("dhiogo.jpg")
