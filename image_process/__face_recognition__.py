87ba22fec3ba50c50b77206cb172029c74a73886
jonas <jonas@itera.com.br>
2019-02-17 01:38:27 -0300

    refatoração/melhoria

--- image_process/__face_recognition__.py
+++ image_process/__face_recognition__.py
@@ -9,45 +9,41 @@ import uuid
 #parametros:
 webcam = 1 # qual webcam usar( 0 = default, 1= webcam usb etc...)
 velocidade_proc = 2 # quanto maior mais rapido o processamento, mas tbm pode pegar menos faces
-path_fotos = ""
-process_this_frame = True
+path_queue = "imgs/queue/{}.png"
 
-def worker(path,frame):    
+
+def thread_write_img(path,frame):    
     cv2.imwrite(path,frame)
 
+def webcam_capture():
+    video_capture = cv2.VideoCapture(0)
+    dhiogo_image = face_recognition.load_image_file("dhiogo.jpg")
+    dhiogo_face_encoding = face_recognition.face_encodings(dhiogo_image)[0]
+    catch_each_10_frames=1
+    while True:
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
+        small_frame = cv2.resize(frame, (0, 0), fx=float(1/velocidade_proc), fy=float(1/velocidade_proc))
+        rgb_small_frame = small_frame[:, :, ::-1]
+        face_locations = face_recognition.face_locations(rgb_small_frame)
 
+        for (top, right, bottom, left) in face_locations:
+            top *=velocidade_proc
+            right *=velocidade_proc
+            bottom *=velocidade_proc
+            left *=velocidade_proc
+            if(catch_each_10_frames==0):
+                multiprocessing.Process(target=thread_write_img,args=(path_queue.format(str(uuid.uuid4())),frame,)).start()
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            catch_each_10_frames = catch_each_10_frames +1
+            if(catch_each_10_frames>5):
+                catch_each_10_frames=0
+        cv2.imshow('Video', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-video_capture = cv2.VideoCapture(0)
-dhiogo_image = face_recognition.load_image_file("dhiogo.jpg")
-dhiogo_face_encoding = face_recognition.face_encodings(dhiogo_image)[0]
-i=1
-while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
-    small_frame = cv2.resize(frame, (0, 0), fx=float(1/velocidade_proc), fy=float(1/velocidade_proc))
-    rgb_small_frame = small_frame[:, :, ::-1]
-    face_locations = face_recognition.face_locations(rgb_small_frame)
-    #multiprocessing.Process(target=worker,args=(face_locations, rgb_small_frame,dhiogo_face_encoding,)).start()
+    # When everything is done, release the capture
+    video_capture.release()
+    cv2.destroyAllWindows()
 
-    for (top, right, bottom, left) in face_locations:
-        top *=velocidade_proc
-        right *=velocidade_proc
-        bottom *=velocidade_proc
-        left *=velocidade_proc
-        #crop_img = frame[top-10:bottom+10, left-10:right+10]
-
-        print(i)
-        if(i==0):
-            multiprocessing.Process(target=worker,args=("imgs/queue/{}.png".format(str(uuid.uuid4())),frame,)).start()
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-        i = i +1
-        
-        if(i>5):
-            i=0
-    cv2.imshow('Video', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything is done, release the capture
-video_capture.release()
-cv2.destroyAllWindows()
+webcam_capture()
--- image_process/process_queue.py
+++ image_process/process_queue.py
@@ -6,59 +6,64 @@ import multiprocessing
 import time
 import os
 from multiprocessing import Pool
+import api
+import json
 
+#parametros
+velocidade_proc = 2
+path= "imgs/queue/"
+event_id = "7ffeb450-c571-4bfe-95cc-362b744d8e06"
 
-def worker(img_full_path): 
-    global imgs_database
+#variavel global
+custumers_encoing_img_database = list()
+
+def match_faces(img_full_path): 
+    global custumers_encoing_img_database
     print(img_full_path)
     img = cv2.imread(img_full_path,1)
     small_img = cv2.resize(img, (0, 0), fx=float(1/velocidade_proc), fy=float(1/velocidade_proc))
     face_locations = face_recognition.face_locations(small_img)
     face_encodings = face_recognition.face_encodings(small_img, face_locations)
     for face_encoding in face_encodings:
-        for img_database in imgs_database:
-            matche = face_recognition.compare_faces([img_database], face_encoding)
+        for img_encoing_database in custumers_encoing_img_database:
+            matche = face_recognition.compare_faces([img_encoing_database['b64']], face_encoding)
             print(matche)
     os.remove(img_full_path)
 
 
+def load_img_from_Database():
+    print("load from database")
+    global imgs_database
+    global event_id
+    headers = {'Content-Type': 'application/json','Master-Key': 'facefyiscool'}
+    json_data = api.get_account_info(headers=headers,event_id=event_id)
+    print('customers')
+    for customers in json_data['customers']:
+        custumers_encoing_img_database.append({'id':customers['customerId'],'b64':customers['base64Photo']})
+        print(customers['customerId'],customers['base64Photo'])
+    #fake data
+    # for i in range(0,100):
+    #     dhiogo_image = face_recognition.load_image_file("dhiogo.jpg")
+    #     small_dhiogo = cv2.resize(dhiogo_image, (0, 0), fx=float(1/4), fy=float(1/4))
+    #     dhiogo_face_encoding = face_recognition.face_encodings(small_dhiogo)[0]
+    #     imgs_database.append(dhiogo_face_encoding)
+    print("finish load from database")
 
-    # face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-    # for face_encoding in face_encodings:
-    #     matche = face_recognition.compare_faces([dhiogo_face_encoding], face_encoding)
-    #     print(matche)
 
-
-velocidade_proc = 2
-imgs_database = list()
-for i in range(0,100):
-    dhiogo_image = face_recognition.load_image_file("dhiogo.jpg")
-    small_dhiogo = cv2.resize(dhiogo_image, (0, 0), fx=float(1/4), fy=float(1/4))
-    dhiogo_face_encoding = face_recognition.face_encodings(small_dhiogo)[0]
-    imgs_database.append(dhiogo_face_encoding)
-print("acabo")
-
-path= "imgs/queue/"
-while(True):
-    if(len(os.listdir(path))==0):
+def infinit_process():
+    print("start infinit process...")
+    while(True):
+        if(len(os.listdir(path))==0):
+            time.sleep(3)
+            continue
+        onlyfiles = [os.path.join(path, f) for f in os.listdir(path) ]
         time.sleep(3)
-        continue
-    onlyfiles = [os.path.join(path, f) for f in os.listdir(path) ]
-    time.sleep(3)
-    p = Pool()
-    p.map(worker, onlyfiles)
-    p.close()
-    p.join()
-    
-    # try:
-    #     for img_path in onlyfiles:
-    #         img_full_path = os.path.join(path, img_path)
-    #         print(img_path)
-    #         img = cv2.imread(img_full_path,1)
-    #         small_img = cv2.resize(img, (0, 0), fx=float(1/velocidade_proc), fy=float(1/velocidade_proc))
-    #         face_locations = face_recognition.face_locations(small_img)
-    #         for img_database in imgs_database:
-    #             worker(face_locations,img,img_database)
-    #         os.remove(img_full_path)
-    # except:
-    #     print("preparando...")
+        p = Pool()
+        p.map(match_faces, onlyfiles)
+        p.close()
+        p.join()
+
+
+
+load_img_from_Database()
+infinit_process()
