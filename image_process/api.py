ca3111b438a2d5c58d1a6e8119e4098338235a8a
jonas <jonas@itera.com.br>
2019-02-17 01:38:35 -0300

    api

--- image_process/api.py
+++ image_process/api.py
@@ -1,20 +1,36 @@
 import json
 import requests
 
+ip = "10.10.0.186"
+porta = "8081"
 
-def get_account_info():
+def get_account_info(headers,event_id):
+    url = 'http://'+ip+':'+porta+'/event/'+event_id
+    #api_url = '{0}account'.format(api_url_base)
+    try:
+        response = requests.get(url, headers=headers)
+        print(response.status_code)
+        if response.status_code == 200:
+            return json.loads(response.content.decode('utf-8'))
+        else:
+            return None
+    except:
+        print('get_account_info, erro na url:',url)
 
-    api_url = '{0}account'.format(api_url_base)
 
-    response = requests.get(api_url, headers=headers)
+def send_confirm_buy(event_id,headers):
+    try:
+        url = 'http://'+ip+':'+porta+'/cashiers/'+event_id
+        url = url+"/"+id_do_evento+"?amount="+valor_pagar+"&description="+descricao
+        r = requests.post(url, data = {},headers=headers)
+    except:
+        print('send_confirm_buy, erro na url:',url)
 
-    if response.status_code == 200:
-        return json.loads(response.content.decode('utf-8'))
-    else:
-        return None
 
-api_token = 'your_api_token'
-event_id = "1"
-api_url_base = 'http://10.10.0.186:8081/events/custumers/'+event_id
-headers = {'Content-Type': 'application/json','Master-Key': 'facefyiscool'}
+# event_id = "7ffeb450-c571-4bfe-95cc-362b744d8e06"
+# headers = {'Content-Type': 'application/json','Master-Key': 'facefyiscool'}
+# print(get_account_info(headers,event_id))
 
+# id_do_carinha = 1
+# headers= {'Custumer-Id':id_do_carinha,'Content-Type':'application/json'}
+# send_confirm_buy(event_id,headers)
