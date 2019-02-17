28db9484b37a562bc3d1747842695e5e5dc7aabc
jonas <jonas@itera.com.br>
2019-02-17 05:00:03 -0300

    ta tudo OK

--- image_process/api.py
+++ image_process/api.py
@@ -1,8 +1,12 @@
 import json
 import requests
-
+import base64
+import face_recognition
+import b64_img
+import cv2
 ip = "10.10.0.186"
 porta = "8081"
+event_id = "7ffeb450-c571-4bfe-95cc-362b744d8e06"
 
 def get_account_info(headers,event_id):
     url = 'http://'+ip+':'+porta+'/event/'+event_id
@@ -18,14 +22,38 @@ def get_account_info(headers,event_id):
         print('get_account_info, erro na url:',url)
 
 
-def send_confirm_buy(event_id,headers):
+def send_confirm_buy(event_id,headers,valor_pagar):
+    
+    url = 'http://'+ip+':'+porta+'/cashier/'
+    url = url+"/"+event_id+"?amount="+valor_pagar+"&description=123123"
     try:
-        url = 'http://'+ip+':'+porta+'/cashiers/'+event_id
-        url = url+"/"+id_do_evento+"?amount="+valor_pagar+"&description="+descricao
         r = requests.post(url, data = {},headers=headers)
+        return r
     except:
         print('send_confirm_buy, erro na url:',url)
+    return False
 
+
+def load_img_from_Database():
+    custumers_encoing_img_database = list()
+    print("load from database")
+    global imgs_database
+    global event_id
+    headers = {'Content-Type': 'application/json','Master-Key': 'facefyiscool'}
+    json_data = get_account_info(headers=headers,event_id=event_id)
+    for customers in json_data['customers']:
+        img_base64 = b64_img.readb64(customers['base64Photo'])
+        if(img_base64 is not None):
+            small_img_detect = cv2.resize(img_base64, (0, 0), fx=float(1/2), fy=float(1/2))
+            face_encoding = face_recognition.face_encodings(small_img_detect)
+            if(len(face_encoding)>0):
+                custumers_encoing_img_database.append({'id':customers['customerId'],'b64':face_encoding[0],'nome':customers['firstName']})
+            else:
+                print(customers['customerId'],' face nao detectada')
+        else:
+            print(customers['customerId'],' nao tem foto correta')
+    print("finish load from database")
+    return custumers_encoing_img_database
 
 # event_id = "7ffeb450-c571-4bfe-95cc-362b744d8e06"
 # headers = {'Content-Type': 'application/json','Master-Key': 'facefyiscool'}
--- image_process/process_queue.py
+++ image_process/process_queue.py
@@ -38,35 +38,25 @@ def match_faces(img_full_path):
 
 
 def load_img_from_Database():
+    custumers_encoing_img_database = list()
     print("load from database")
     global imgs_database
     global event_id
     headers = {'Content-Type': 'application/json','Master-Key': 'facefyiscool'}
     json_data = api.get_account_info(headers=headers,event_id=event_id)
     for customers in json_data['customers']:
-        print(customers['base64Photo'])
         img_base64 = b64_img.readb64(customers['base64Photo'])
         if(img_base64 is not None):
             small_img_detect = cv2.resize(img_base64, (0, 0), fx=float(1/velocidade_proc), fy=float(1/velocidade_proc))
             face_encoding = face_recognition.face_encodings(small_img_detect)
             if(len(face_encoding)>0):
-                print('TAMANHO: ',len(face_encoding))
                 custumers_encoing_img_database.append({'id':customers['customerId'],'b64':face_encoding[0],'nome':customers['firstName']})
             else:
-                dhiogo_image = face_recognition.load_image_file("dhiogo.jpg")
-                small_dhiogo = cv2.resize(dhiogo_image, (0, 0), fx=float(1/4), fy=float(1/4))
-                dhiogo_face_encoding = face_recognition.face_encodings(small_dhiogo)[0]
-                custumers_encoing_img_database.append({'id':'dhiogo','b64':dhiogo_face_encoding,'nome':'dhiogo'})
                 print(customers['customerId'],' face nao detectada')
         else:
             print(customers['customerId'],' nao tem foto correta')
-    #fake data
-    # for i in range(0,100):
-    #     dhiogo_image = face_recognition.load_image_file("dhiogo.jpg")
-    #     small_dhiogo = cv2.resize(dhiogo_image, (0, 0), fx=float(1/4), fy=float(1/4))
-    #     dhiogo_face_encoding = face_recognition.face_encodings(small_dhiogo)[0]
-    #     imgs_database.append(dhiogo_face_encoding)
     print("finish load from database")
+    return custumers_encoing_img_database
 
 
 def infinit_process():
@@ -84,5 +74,5 @@ def infinit_process():
 
 
 
-load_img_from_Database()
+custumers_encoing_img_database = api.load_img_from_Database()
 infinit_process()
